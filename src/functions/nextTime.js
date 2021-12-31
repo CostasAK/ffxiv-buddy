80158c8062ad7dfc5a4749ae64d608602769246d
CostasAK <costas@kokke.eu>
2021-12-31 21:24:16 +0100

    offset on now in case the timer quits a few milliseconds early

--- src/functions/nextTime.js
+++ src/functions/nextTime.js
@@ -2,7 +2,7 @@ export function nextTime(period, time) {
   if (!period || !time) {
     return time;
   }
-  let now = Date.now();
+  let now = Date.now() + 1;
   let offset = time % period;
   let remainder = now % period || 0;
   if (remainder < offset) {
