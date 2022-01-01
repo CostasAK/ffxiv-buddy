4fce57859f1feab1817130edd7284b8c1cda3b1b
CostasAK <costas@kokke.eu>
2022-01-01 14:19:39 +0100

    use flooring and ceiling instead of offset, to whole seconds

--- src/functions/nextTime.js
+++ src/functions/nextTime.js
@@ -2,7 +2,11 @@ export function nextTime(period, time) {
   if (!period || !time) {
     return time;
   }
-  let now = Date.now() + 1;
+
+  let now = Date.now() + 1000;
+  now += 1000 - (now % 1000);
+  time -= time % 1000;
+
   let offset = time % period;
   let remainder = now % period || 0;
   if (remainder < offset) {
