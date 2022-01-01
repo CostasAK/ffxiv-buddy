115e3fe73972228666106aa1dadba0c7046f5fa0
Costas <23523864+CostasAK@users.noreply.github.com>
2022-01-01 15:14:32 +0100

    Update getResets.js
    removed generic repeatable quests. Need to actually find some before putting those in.

--- src/functions/getResets.js
+++ src/functions/getResets.js
@@ -9,7 +9,7 @@ export function getResets() {
     {
       name: "Weekly Reset",
       description:
-        "The following things reset at this time:<ul><li>Weekly repeatable quests.</li><li>Weekly tomestone cap.</li><li>Elite mark bills.</li><li>Masked Carnivale.</li><li>Challenge Log.</li><li>Wondrous Tails.</li><li>Faux Hollows.</li><li>Custom Deliveries.</li><li>Doman Enclave Reconstruction Effort.</li><li>Squadron Priority Mission.</li></ul>",
+        "The following things reset at this time:<ul><li>Weekly tomestone cap.</li><li>Elite mark bills.</li><li>Masked Carnivale.</li><li>Challenge Log.</li><li>Wondrous Tails.</li><li>Faux Hollows.</li><li>Custom Deliveries.</li><li>Doman Enclave Reconstruction Effort.</li><li>Squadron Priority Mission.</li></ul>",
       period: week,
       start: new Date("28 December 2021 8:00 GMT").getTime(),
       type: "reset",
