0a6e6237b4f8aecfcff78d8058861ea6fd6357cc
CostasAK <costas@kokke.eu>
2022-01-04 08:45:38 +0100

    raid and tomestone specified

--- src/functions/getResets.js
+++ src/functions/getResets.js
@@ -9,7 +9,7 @@ export function getResets() {
     {
       name: "Weekly Reset",
       description:
-        "The following things reset at this time:<ul><li>Weekly tomestone cap.</li><li>Normal Raid rewards.</li><li>Elite mark bills.</li><li>Masked Carnivale.</li><li>Challenge Log.</li><li>Wondrous Tails.</li><li>Faux Hollows.</li><li>Custom Deliveries.</li><li>Doman Enclave Reconstruction Effort.</li><li>Squadron Priority Mission.</li></ul>",
+        "The following things reset at this time:<ul><li>Weekly Allagan tomestones of astronomy cap.</li><li>Asphodelos (Normal) raid rewards.</li><li>Asphodelos (Savage) raid rewards and progression.</li><li>Elite mark bills.</li><li>Masked Carnivale.</li><li>Challenge Log.</li><li>Wondrous Tails.</li><li>Faux Hollows.</li><li>Custom Deliveries.</li><li>Doman Enclave Reconstruction Effort.</li><li>Squadron Priority Mission.</li></ul>",
       period: week,
       start: new Date("28 December 2021 8:00 GMT").getTime(),
       type: "reset",
