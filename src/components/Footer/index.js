9691586a58ef179abba3cee9495a3f157ea18d82
CostasAK <costas@kokke.eu>
2022-01-10 08:20:04 +0100

    added local and eorzea time to footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,11 +4,56 @@ import { Component } from "react";
 import { FaGithub } from "react-icons/fa";
 import { SiKofi } from "react-icons/si";
 import avatar from "../../assets/initialen.svg";
+import { eorzeaTime } from "../../functions/eorzeaTime";
+import { formatTime } from "../../functions/formatDateTime";
 
 export class Footer extends Component {
+  constructor(props) {
+    super(props);
+
+    let now = Date.now();
+
+    this.state = {
+      eorzeaTime: eorzeaTime(now),
+      localTime: formatTime(now),
+    };
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      let now = Date.now();
+      this.setState({
+        eorzeaTime: eorzeaTime(now),
+        localTime: formatTime(now),
+      });
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
   render() {
     return (
       <footer className="footer">
+        <div className="footer-time">
+          <div>
+            <span>
+              <b>ET</b>
+            </span>
+            <span className="time">
+              {this.state.eorzeaTime.padStart(5, "0")}
+            </span>
+          </div>
+          <div>
+            <span>
+              <b>LT</b>
+            </span>
+            <span className="time">
+              {this.state.localTime.padStart(5, "0")}
+            </span>
+          </div>
+        </div>
         <div className="footer-credits">
           <div>
             <span>Made by</span>  
