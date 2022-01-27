546cb91ea2f2b7d239a8b0cf482ecdeefc6b7c79
CostasAK <costas@kokke.eu>
2022-01-27 10:49:44 +0100

    small refactor of todos and categories and fixed initial amount of columns

--- src/components/Checklist/index.js
+++ src/components/Checklist/index.js
@@ -7,82 +7,159 @@ const second = 1000;
 const minute = 60 * second;
 const hour = 60 * minute;
 
-const categories = [
-  "Collectables",
-  "Roulettes",
-  "Raids",
-  "Gold Saucer",
-  // "Housing",
-  // "DoH/L",
-  "Grand Company and Hunts",
-];
-
 const todos = [
-  { name: "Weekly Tomestones", reset: "weekly", category: "Collectables" },
   {
-    name: "Asphodelos (Normal) rewards",
-    reset: "weekly",
-    category: "Raids",
-  },
-  { name: "Asphodelos (Savage) rewards", reset: "weekly", category: "Raids" },
-  {
-    name: "Elite mark bills",
-    reset: "weekly",
-    category: "Grand Company and Hunts",
-  },
-  { name: "Masked Carnivale", reset: "weekly" },
-  { name: "Challenge Log", reset: "weekly" },
-  { name: "Wondrous Tails", reset: "weekly" },
-  { name: "Faux Hollows", reset: "weekly", category: "Raids" },
-  { name: "Custom Deliveries", reset: "weekly" },
-  {
-    name: "Doman Enclave Reconstruction Effort",
-    reset: "weekly",
     category: "Collectables",
+    todos: [
+      {
+        name: "Weekly Tomestones",
+        reset: "weekly",
+      },
+      {
+        name: "Treasure Map",
+        period: 18 * hour,
+      },
+    ],
   },
   {
-    name: "Squadron Priority Mission",
-    reset: "weekly",
-    category: "Grand Company and Hunts",
+    category: "Raids",
+    todos: [
+      {
+        name: "Asphodelos (Normal) rewards",
+        reset: "weekly",
+      },
+      {
+        name: "Asphodelos (Savage) rewards",
+        reset: "weekly",
+      },
+      {
+        name: "Faux Hollows",
+        reset: "weekly",
+      },
+    ],
   },
-  { name: "Beast Tribe Quests", reset: "daily" },
   {
-    name: "Regular Mark Bills",
-    reset: "daily",
-    category: "Grand Company and Hunts",
-  },
-  { name: "Expert Roulette", reset: "daily", category: "Roulettes" },
-  {
-    name: "Level 50/60/70/80 Dungeons Roulette",
-    reset: "daily",
     category: "Roulettes",
+    todos: [
+      {
+        name: "Expert Roulette",
+        reset: "daily",
+      },
+      {
+        name: "Level 50/60/70/80 Dungeons Roulette",
+        reset: "daily",
+      },
+      {
+        name: "Leveling Roulette",
+        reset: "daily",
+      },
+      {
+        name: "Trials Roulette",
+        reset: "daily",
+      },
+      {
+        name: "Main Scenario Roulette",
+        reset: "daily",
+      },
+      {
+        name: "Guildhests Roulette",
+        reset: "daily",
+      },
+      {
+        name: "Alliance Raids Roulette",
+        reset: "daily",
+      },
+      {
+        name: "Normal Raids Roulette",
+        reset: "daily",
+      },
+      {
+        name: "Mentor Roulette",
+        reset: "daily",
+      },
+      {
+        name: "Frontline Roulette",
+        reset: "daily",
+      },
+    ],
   },
-  { name: "Leveling Roulette", reset: "daily", category: "Roulettes" },
-  { name: "Trials Roulette", reset: "daily", category: "Roulettes" },
-  { name: "Main Scenario Roulette", reset: "daily", category: "Roulettes" },
-  { name: "Guildhests Roulette", reset: "daily", category: "Roulettes" },
-  { name: "Alliance Raids Roulette", reset: "daily", category: "Roulettes" },
-  { name: "Normal Raids Roulette", reset: "daily", category: "Roulettes" },
-  { name: "Mentor Roulette", reset: "daily", category: "Roulettes" },
-  { name: "Frontline Roulette", reset: "daily", category: "Roulettes" },
-  { name: "Mini Cactpot", reset: "daily", category: "Gold Saucer" },
   {
-    name: "Grand Company Turn-ins",
-    reset: "grand company",
     category: "Grand Company and Hunts",
+    todos: [
+      {
+        name: "Grand Company Turn-ins",
+        reset: "grand company",
+      },
+      {
+        name: "Squadron Priority Mission",
+        reset: "weekly",
+      },
+      {
+        name: "Squadron Mission",
+        period: 18 * hour,
+      },
+      {
+        name: "Elite mark bills",
+        reset: "weekly",
+      },
+      {
+        name: "Regular Mark Bills",
+        reset: "daily",
+      },
+    ],
   },
-  { name: "Fashion Report", reset: "fashion report", category: "Gold Saucer" },
-  { name: "Jumbo Cactpot", reset: "jumbo cactpot", category: "Gold Saucer" },
-  { name: "Treasure Map", period: 18 * hour, category: "Collectables" },
   {
-    name: "Squadron Mission",
-    period: 18 * hour,
-    category: "Grand Company and Hunts",
+    category: "Gold Saucer",
+    todos: [
+      {
+        name: "Mini Cactpot",
+        reset: "daily",
+      },
+      {
+        name: "Fashion Report",
+        reset: "fashion report",
+      },
+      {
+        name: "Jumbo Cactpot",
+        reset: "jumbo cactpot",
+      },
+    ],
+  },
+  {
+    category: "Others",
+    todos: [
+      {
+        name: "Masked Carnivale",
+        reset: "weekly",
+      },
+      {
+        name: "Challenge Log",
+        reset: "weekly",
+      },
+      {
+        name: "Wondrous Tails",
+        reset: "weekly",
+      },
+      {
+        name: "Custom Deliveries",
+        reset: "weekly",
+      },
+      {
+        name: "Doman Enclave Reconstruction Effort",
+        reset: "weekly",
+      },
+      {
+        name: "Beast Tribe Quests",
+        reset: "daily",
+      },
+    ],
   },
 ];
 
 export function Checklist() {
-  const [columns, setColumns] = React.useState(1);
+  const [columns, setColumns] = React.useState(
+    Math.floor(window.innerWidth / 470)
+  );
 
   React.useEffect(() => {
     window.addEventListener("resize", () => {
@@ -92,33 +169,13 @@ export function Checklist() {
 
   return (
     <div className="Checklist" style={{ columns: columns }}>
-      {categories.map((c) => (
+      {todos.map((c) => (
         <div className="todo-category">
           <div className="category-header">
-            <span>{c}</span>
+            <span>{c.category}</span>
           </div>
           <div className="todos">
-            {todos
-              .filter((t) => t.category === c)
-              .map((t) => (
-                <ToDo
-                  name={t.name}
-                  reset={t.reset}
-                  period={t.period}
-                  key={t.name}
-                />
-              ))}
-          </div>
-        </div>
-      ))}
-      <div className="todo-category">
-        <div className="category-header">
-          <span>Others</span>
-        </div>
-        <div className="todos">
-          {todos
-            .filter((t) => !t.category)
-            .map((t) => (
+            {c.todos.map((t) => (
               <ToDo
                 name={t.name}
                 reset={t.reset}
@@ -126,11 +183,9 @@ export function Checklist() {
                 key={t.name}
               />
             ))}
+          </div>
         </div>
-      </div>
-      {/* {todos.map((t) => (
-        <ToDo name={t.name} reset={t.reset} period={t.period} key={t.name} />
-      ))} */}
+      ))}
     </div>
   );
 }
