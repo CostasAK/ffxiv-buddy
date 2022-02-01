832e1af4c241fe9d4cb27cfd195f31b21544e883
CostasAK <costas@kokke.eu>
2022-02-01 14:34:39 +0100

    implemented masonry for checklist categories using css grid and scrollheight. refactor todos to App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,203 @@ import { Header } from "./components/Header";
 import { Navigation } from "./components/Navigation";
 import React from "react";
 import { Timers } from "./components/Timers";
+import collectables_icon from "./assets/chat_messengericon_treasurehunt.png";
+import goldsaucer_icon from "./assets/chat_messengericon_goldsaucer.png";
+import hunts_icon from "./assets/chat_messengericon_thehunt.png";
+import others_icon from "./assets/chat_messengericon_weeklybingo.png";
+import raid_icon from "./assets/chat_messengericon_raids.png";
+import roulette_icon from "./assets/chat_messengericon_dutyroulette.png";
+
+const second = 1000;
+const minute = 60 * second;
+const hour = 60 * minute;
+
+const todos = [
+  {
+    category: "Collectables",
+    icon: collectables_icon,
+    todos: [
+      {
+        name: "Allagan Tomestones of Astronomy",
+        reset: "weekly",
+      },
+      {
+        name: "Treasure Map",
+        period: 18 * hour,
+      },
+    ],
+  },
+  {
+    category: "Raids",
+    icon: raid_icon,
+    todos: [
+      {
+        name: "Asphodelos: The First Circle (Normal)",
+        reset: "weekly",
+      },
+      {
+        name: "Asphodelos: The Second Circle (Normal)",
+        reset: "weekly",
+      },
+      {
+        name: "Asphodelos: The Third Circle (Normal)",
+        reset: "weekly",
+      },
+      {
+        name: "Asphodelos: The Fourth Circle (Normal)",
+        reset: "weekly",
+      },
+      {
+        name: "Unsung Blade of Asphodelos",
+        reset: "weekly",
+      },
+      {
+        name: "Asphodelos: The First Circle (Savage)",
+        reset: "weekly",
+      },
+      {
+        name: "Asphodelos: The Second Circle (Savage)",
+        reset: "weekly",
+      },
+      {
+        name: "Asphodelos: The Third Circle (Savage)",
+        reset: "weekly",
+      },
+      {
+        name: "Asphodelos: The Fourth Circle (Savage)",
+        reset: "weekly",
+      },
+      {
+        name: "Faux Hollows",
+        reset: "weekly",
+      },
+    ],
+  },
+  {
+    category: "Roulettes",
+    icon: roulette_icon,
+    todos: [
+      {
+        name: "Expert Roulette",
+        reset: "daily",
+      },
+      {
+        name: "Level 50/60/70/80 Dungeons Roulette",
+        reset: "daily",
+      },
+      {
+        name: "Leveling Roulette",
+        reset: "daily",
+      },
+      {
+        name: "Trials Roulette",
+        reset: "daily",
+      },
+      {
+        name: "Main Scenario Roulette",
+        reset: "daily",
+      },
+      {
+        name: "Guildhests Roulette",
+        reset: "daily",
+      },
+      {
+        name: "Alliance Raids Roulette",
+        reset: "daily",
+      },
+      {
+        name: "Normal Raids Roulette",
+        reset: "daily",
+      },
+      {
+        name: "Mentor Roulette",
+        reset: "daily",
+      },
+      {
+        name: "Frontline Roulette",
+        reset: "daily",
+      },
+    ],
+  },
+  {
+    category: "Grand Company and Hunts",
+    icon: hunts_icon,
+    todos: [
+      {
+        name: "Grand Company Turn-ins",
+        reset: "grand company",
+      },
+      {
+        name: "Squadron Priority Mission",
+        reset: "weekly",
+      },
+      {
+        name: "Squadron Mission",
+        period: 18 * hour,
+      },
+      {
+        name: "Elite Mark Bills",
+        reset: "weekly",
+      },
+      {
+        name: "Regular Mark Bills",
+        reset: "daily",
+      },
+    ],
+  },
+  {
+    category: "Gold Saucer",
+    icon: goldsaucer_icon,
+    todos: [
+      {
+        name: "Mini Cactpot",
+        reset: "daily",
+      },
+      {
+        name: "Fashion Report",
+        reset: "fashion report",
+      },
+      {
+        name: "Jumbo Cactpot",
+        reset: "jumbo cactpot",
+      },
+      {
+        name: "Triple Triad / Verminion Tournament",
+        reset: "weekly",
+      },
+    ],
+  },
+  {
+    category: "Others",
+    icon: others_icon,
+    todos: [
+      {
+        name: "Masked Carnivale",
+        reset: "weekly",
+      },
+      {
+        name: "Challenge Log",
+        reset: "weekly",
+      },
+      {
+        name: "Wondrous Tails",
+        reset: "weekly",
+      },
+      {
+        name: "Custom Deliveries",
+        reset: "weekly",
+      },
+      {
+        name: "Doman Enclave Reconstruction Effort",
+        reset: "weekly",
+      },
+      {
+        name: "Beast Tribe Quests",
+        reset: "daily",
+      },
+    ],
+  },
+];
 
 function App() {
   const all_pages = ["Timers", "Checklist"];
@@ -18,10 +215,11 @@ function App() {
   }, [page]);
 
   const Page = (props) => {
+    const { page, ...leftovers } = props;
     if (props.page === "Checklist") {
-      return <Checklist />;
+      return <Checklist {...leftovers} />;
     }
-    return <Timers />;
+    return <Timers {...leftovers} />;
   };
 
   return (
@@ -29,7 +227,7 @@ function App() {
       <Header />
       <Navigation setPage={setPage} getPage={page} allPages={all_pages} />
       <main className="main">
-        <Page page={page} />
+        <Page page={page} todos={todos} />
       </main>
 
       <Footer />
--- src/components/Checklist/index.js
+++ src/components/Checklist/index.js
@@ -1,186 +1,50 @@
 import "./style.css";
 
-import React from "react";
+import React, { useLayoutEffect, useRef, useState } from "react";
+
 import { ToDo } from "../ToDo";
-import collectables_icon from "../../assets/chat_messengericon_treasurehunt.png";
-import goldsaucer_icon from "../../assets/chat_messengericon_goldsaucer.png";
-import hunts_icon from "../../assets/chat_messengericon_thehunt.png";
-import others_icon from "../../assets/chat_messengericon_weeklybingo.png";
-import raid_icon from "../../assets/chat_messengericon_raids.png";
-import roulette_icon from "../../assets/chat_messengericon_dutyroulette.png";
 
-const second = 1000;
-const minute = 60 * second;
-const hour = 60 * minute;
+function getSpanHeight(ref) {
+  let rowHeight = parseInt(
+    window
+      .getComputedStyle(ref.current.parentNode)
+      .getPropertyValue("grid-auto-rows")
+  );
+  let rowGap = parseInt(
+    window
+      .getComputedStyle(ref.current.parentNode)
+      .getPropertyValue("grid-row-gap")
+  );
+  return Math.ceil((ref.current.scrollHeight + rowGap) / (rowHeight + rowGap));
+}
 
-const todos = [
-  {
-    category: "Collectables",
-    icon: collectables_icon,
-    todos: [
-      {
-        name: "Weekly Tomestones",
-        reset: "weekly",
-      },
-      {
-        name: "Treasure Map",
-        period: 18 * hour,
-      },
-    ],
-  },
-  {
-    category: "Raids",
-    icon: raid_icon,
-    todos: [
-      {
-        name: "Asphodelos (Normal) rewards",
-        reset: "weekly",
-      },
-      {
-        name: "Asphodelos (Savage) rewards",
-        reset: "weekly",
-      },
-      {
-        name: "Faux Hollows",
-        reset: "weekly",
-      },
-    ],
-  },
-  {
-    category: "Roulettes",
-    icon: roulette_icon,
-    todos: [
-      {
-        name: "Expert Roulette",
-        reset: "daily",
-      },
-      {
-        name: "Level 50/60/70/80 Dungeons Roulette",
-        reset: "daily",
-      },
-      {
-        name: "Leveling Roulette",
-        reset: "daily",
-      },
-      {
-        name: "Trials Roulette",
-        reset: "daily",
-      },
-      {
-        name: "Main Scenario Roulette",
-        reset: "daily",
-      },
-      {
-        name: "Guildhests Roulette",
-        reset: "daily",
-      },
-      {
-        name: "Alliance Raids Roulette",
-        reset: "daily",
-      },
-      {
-        name: "Normal Raids Roulette",
-        reset: "daily",
-      },
-      {
-        name: "Mentor Roulette",
-        reset: "daily",
-      },
-      {
-        name: "Frontline Roulette",
-        reset: "daily",
-      },
-    ],
-  },
-  {
-    category: "Grand Company and Hunts",
-    icon: hunts_icon,
-    todos: [
-      {
-        name: "Grand Company Turn-ins",
-        reset: "grand company",
-      },
-      {
-        name: "Squadron Priority Mission",
-        reset: "weekly",
-      },
-      {
-        name: "Squadron Mission",
-        period: 18 * hour,
-      },
-      {
-        name: "Elite Mark Bills",
-        reset: "weekly",
-      },
-      {
-        name: "Regular Mark Bills",
-        reset: "daily",
-      },
-    ],
-  },
-  {
-    category: "Gold Saucer",
-    icon: goldsaucer_icon,
-    todos: [
-      {
-        name: "Mini Cactpot",
-        reset: "daily",
-      },
-      {
-        name: "Fashion Report",
-        reset: "fashion report",
-      },
-      {
-        name: "Jumbo Cactpot",
-        reset: "jumbo cactpot",
-      },
-    ],
-  },
-  {
-    category: "Others",
-    icon: others_icon,
-    todos: [
-      {
-        name: "Masked Carnivale",
-        reset: "weekly",
-      },
-      {
-        name: "Challenge Log",
-        reset: "weekly",
-      },
-      {
-        name: "Wondrous Tails",
-        reset: "weekly",
-      },
-      {
-        name: "Custom Deliveries",
-        reset: "weekly",
-      },
-      {
-        name: "Doman Enclave Reconstruction Effort",
-        reset: "weekly",
-      },
-      {
-        name: "Beast Tribe Quests",
-        reset: "daily",
-      },
-    ],
-  },
-];
+function TodoCategory(props) {
+  const ref = useRef(null);
+  const [height, setHeight] = useState(0);
 
-export function Checklist() {
-  const getColumns = () =>
-    Math.max(Math.min(Math.floor(window.innerWidth / 480), 3), 1);
-  const [columns, setColumns] = React.useState(getColumns());
-
-  React.useEffect(() => {
-    window.addEventListener("resize", () => setColumns(getColumns()));
-  }, []);
+  useLayoutEffect(() => {
+    setHeight(getSpanHeight(ref));
+    window.addEventListener("resize", () => {
+      setHeight(getSpanHeight(ref));
+    });
+  }, [props.children]);
 
   return (
-    <div className="Checklist" style={{ columns: columns }}>
-      {todos.map((c) => (
-        <div className="todo-category">
+    <div
+      ref={ref}
+      className="todo-category"
+      style={{ gridRowEnd: "span " + height }}
+    >
+      {props.children}
+    </div>
+  );
+}
+
+export function Checklist(props) {
+  return (
+    <div className="Checklist">
+      {props.todos.map((c) => (
+        <TodoCategory key={c.category}>
           <div className="category-header">
             <img alt="" src={c.icon} />
             <span> {c.category}</span>
@@ -195,7 +59,7 @@ export function Checklist() {
               />
             ))}
           </div>
-        </div>
+        </TodoCategory>
       ))}
     </div>
   );
