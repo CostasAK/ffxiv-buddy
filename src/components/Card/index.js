91d9c96743e9da97fa2a6cc5dfae55af900aa93f
CostasAK <costas@kokke.eu>
2022-02-02 12:21:36 +0100

    refactor card to functional component and removed react-countdown

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,14 +1,14 @@
 import "./style.css";
 
+import React, { useEffect, useState } from "react";
 import { formatDate, formatTime } from "../../functions/formatDateTime";
 
-import Countdown from "react-countdown";
-import React from "react";
+import { capitalizeSentence } from "../../functions/capitalizeSentence";
+import { formatDuration } from "../../functions/formatDuration";
 import { isPast } from "../../functions/isPast";
 import maintenance_icon from "../../assets/maintenance.png";
 import { nextTime } from "../../functions/nextTime";
 import parse from "html-react-parser";
-import { toNaturalLanguageTime } from "../../functions/toNaturalLanguageTime";
 import { toTime } from "../../functions/toTime";
 import topics_icon from "../../assets/topics.png";
 import updates_icon from "../../assets/updates.png";
@@ -25,149 +25,111 @@ const icons = {
   reset: updates_icon,
 };
 
-export class Card extends React.Component {
-  constructor(props) {
-    super(props);
-
-    let start = nextTime(this.props.period, toTime(this.props.start));
-    let end = nextTime(this.props.period, toTime(this.props.end));
-
-    while (this.props.period && end && start > end) {
-      start -= this.props.period;
+export function Card(props) {
+  const [end, setEnd] = useState(nextTime(props.period, toTime(props.end)));
+  const [start, setStart] = useState(() => {
+    let start = nextTime(props.period, toTime(props.start));
+    while (props.period && end && start > end) {
+      start -= props.period;
     }
+    return start;
+  });
+  const [now, setNow] = useState(Date.now());
 
-    this.state = {
-      start: start,
-      end: end,
-      collapsed: true,
-    };
+  const [collapsed, setCollapsed] = useState(true);
 
-    this.onCompleteCountdown = this.onCompleteCountdown.bind(this);
-  }
-
-  onCompleteCountdown() {
-    if (this.props.period) {
-      let start = nextTime(this.props.period, toTime(this.state.start));
-      let end = nextTime(this.props.period, toTime(this.state.end));
-
-      while (this.props.period && end && start >= end) {
-        start -= this.props.period;
-      }
-
-      this.setState({
-        start: start,
-        end: end,
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setEnd(nextTime(props.period, toTime(end)));
+      setNow(() => Date.now());
+      setStart(() => {
+        let start_temp = nextTime(props.period, toTime(start));
+        while (props.period && end && start_temp >= end) {
+          start_temp -= props.period;
+        }
+        return start_temp;
       });
-    }
+    }, 1000);
+    return () => clearTimeout(timer);
+  }, [now, start, end, props.period]);
+
+  let is_recurring = !!props.period;
+
+  let started = false;
+  if (isPast(start)) {
+    started = true;
   }
 
-  render() {
-    let start = this.state.start;
-    let end = this.state.end;
-    let is_recurring = !!this.props.period;
+  let ended = false;
+  if (
+    isPast(end) ||
+    (!end && isPast(start + day)) ||
+    (!is_recurring && (isPast(end) || (!end && isPast(start + day))))
+  ) {
+    ended = true;
+  }
 
-    let ended = false;
-    if (isPast(end) || (!end && isPast(start + day))) {
-      ended = true;
-    }
-    if (ended) {
-      return null;
-    }
+  if (ended) {
+    return null;
+  }
 
-    let started = false;
-    if (isPast(start)) {
-      started = true;
-    }
+  let target_time = start;
+  if (started && end) {
+    target_time = end;
+  }
 
-    if (!is_recurring && (isPast(end) || (!end && isPast(start + day)))) {
-      this.setState({ ended: true });
-      return null;
-    }
+  let countdown = formatDuration(target_time - now);
 
-    let target_time = start;
-    if (started && end) {
-      target_time = end;
-    }
+  let absolute_time_string;
+  if (started && !end) {
+    absolute_time_string = "On " + formatDate(target_time, props.hasTime);
+  } else if (isPast(target_time - day)) {
+    absolute_time_string = "at " + formatTime(target_time, props.hasTime);
+  } else {
+    absolute_time_string = "on " + formatDate(target_time, props.hasTime);
+  }
 
-    let countdown = (
-      <span>
-        {!end ? "In " : started ? "Ends in " : "Starts in "}
-        <Countdown
-          date={target_time}
-          key={[this.props.name, this.props.period, target_time].join(",")}
-          overtime={true}
-          renderer={(props) => toNaturalLanguageTime(props.total)}
-          onComplete={() => this.onCompleteCountdown()}
-        />
-        ,{" "}
-      </span>
-    );
+  let flex_order = target_time / minute;
+  if (!is_recurring) {
+    flex_order -= (10 * year) / minute;
+  }
 
-    let absolute_time_string;
-    if (started && !end) {
-      absolute_time_string =
-        "On " + formatDate(target_time, this.props.hasTime);
-    } else if (isPast(target_time - day)) {
-      absolute_time_string =
-        "at " + formatTime(target_time, this.props.hasTime);
-    } else {
-      absolute_time_string =
-        "on " + formatDate(target_time, this.props.hasTime);
-    }
+  let description =
+    typeof props.description === "string" || props.description instanceof String
+      ? parse(props.description)
+      : props.description;
 
-    let description =
-      typeof this.props.description === "string" ||
-      this.props.description instanceof String
-        ? parse(this.props.description)
-        : this.props.description;
-
-    let flex_order = target_time / minute;
-    if (!is_recurring) {
-      flex_order -= (10 * year) / minute;
-    }
-
-    return (
+  return (
+    <div
+      className={
+        "Card" +
+        (started ? " ongoing" : "") +
+        (collapsed ? " collapsed" : "") +
+        " " +
+        props.type
+      }
+      style={{
+        order: flex_order,
+      }}
+      onClick={() => setCollapsed(!collapsed)}
+    >
+      <h2>
+        <img src={icons[props.type]} alt={props.type} width="22px" />
+        {props.name}
+      </h2>
+      <p>
+        {capitalizeSentence(
+          (is_recurring || !(started && !end) ? countdown + ", " : "") +
+            absolute_time_string
+        )}
+      </p>
       <div
         className={
-          "Card" +
-          (started ? " ongoing" : "") +
-          (this.state.collapsed ? " collapsed" : "") +
-          " " +
-          this.props.type
+          "description" + (collapsed || !props.description ? " collapsed" : "")
         }
-        style={{ order: flex_order }}
-        onClick={() => this.setState({ collapsed: !this.state.collapsed })}
       >
-        <h2>
-          <img
-            src={icons[this.props.type]}
-            alt={this.props.type}
-            width="22px"
-          />
-          {this.props.name}
-        </h2>
-        <p>
-          {is_recurring || !(started && !end) ? countdown : ""}
-          {absolute_time_string}
-        </p>
-        <div
-          className={
-            "description" +
-            (this.state.collapsed || !this.props.description
-              ? " collapsed"
-              : "")
-          }
-        >
-          {description}
-        </div>
+        {description}
       </div>
-    );
-  }
+    </div>
+  );
 }
-
-Card.defaultProps = {
-  end: false,
-  period: false,
-  hasTime: true,
-  description: false,
-};
