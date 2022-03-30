ea9f6ac79cfb5e099471403ac2411ab3f2038dc3
CostasAK <costas@kokke.eu>
2022-03-30 21:51:39 +0200

    implemented react-kofi-button

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,8 +2,8 @@ import "./style.css";
 
 import { Component } from "react";
 import { FaGithub } from "react-icons/fa";
+import { KofiButton } from "react-kofi-button";
 import SVG from "react-inlinesvg";
-import { SiKofi } from "react-icons/si";
 import avatar from "../../assets/initialen.svg";
 import { eorzeaTime } from "../../functions/eorzeaTime";
 import { formatTime } from "../../functions/formatDateTime";
@@ -62,21 +62,7 @@ export class Footer extends Component {
               <SVG src={avatar} className="avatar" /> <span>CostasAK</span>
             </a>
           </div>
-          <div
-            style={{
-              background: "#00B9FE",
-              padding: "calc(var(--margins) / 8) calc(var(--margins) / 2)",
-            }}
-          >
-            <a
-              href="https://ko-fi.com/costasak"
-              style={{ color: "#FFFFFF", fontWeight: "bold" }}
-              target="_blank"
-              rel="noreferrer"
-            >
-              <SiKofi /> <span>Support me</span>
-            </a>
-          </div>
+          <KofiButton username="costasak" preset="skinny" label="Support me" />
           <div>
             <a href="https://github.com/CostasAK/ffxiv-buddy">
               <FaGithub /> <span>Source</span>
