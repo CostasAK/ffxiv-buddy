e44d17f4f23f8bbbf588b249820993e6bfc4e759
CostasAK <costas@kokke.eu>
2024-07-07 22:56:27 +0200

    fix: use biggest icon for header

--- src/layout/header.jsx
+++ src/layout/header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <header className="flex items-center justify-center gap-2 bg-neutral-900 p-2 sm:gap-4 sm:p-4">
-      <img src="/android-chrome-72x72.png" alt="" className="h-8 sm:h-14"></img>
+      <img src="/apple-touch-icon.png" alt="" className="h-8 sm:h-14"></img>
       <h1 className="text-4xl font-bold leading-none sm:text-6xl">
         FFXIV Buddy
       </h1>
