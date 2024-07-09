ee6a5c7ccc84fe201d9c72230c688be6e5cb8530
CostasAK <costas@kokke.eu>
2024-07-09 11:44:31 +0200

    feat: add highres mainicon

--- src/layout/header.jsx
+++ src/layout/header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <header className="flex items-center justify-center gap-2 bg-neutral-900 p-2 text-neutral-200 sm:gap-4 sm:p-4">
-      <img src="/apple-touch-icon.png" alt="" className="h-8 sm:h-14"></img>
+      <img src="/000047_hr1.png" alt="" className="h-8 sm:h-14"></img>
       <h1 className="text-4xl font-bold leading-none sm:text-6xl">
         FFXIV Buddy
       </h1>
