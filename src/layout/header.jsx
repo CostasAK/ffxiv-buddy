914c1e8aee76579a13669cd50519b358ad72f9d5
CostasAK <costas@kokke.eu>
2024-07-09 10:16:57 +0200

    feat: brighter title color

--- src/layout/header.jsx
+++ src/layout/header.jsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <header className="flex items-center justify-center gap-2 bg-neutral-900 p-2 sm:gap-4 sm:p-4">
+    <header className="flex items-center justify-center gap-2 bg-neutral-900 p-2 text-neutral-200 sm:gap-4 sm:p-4">
       <img src="/apple-touch-icon.png" alt="" className="h-8 sm:h-14"></img>
       <h1 className="text-4xl font-bold leading-none sm:text-6xl">
         FFXIV Buddy
