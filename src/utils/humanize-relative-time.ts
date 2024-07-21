2eb9eaaf61efa19f8c70423b2f5efe18b54386e6
CostasAK <costas@kokke.eu>
2024-07-21 22:04:56 +0200

    feat: override dayjs humanize for short durations

--- src/utils/humanize-relative-time.ts
+++ src/utils/humanize-relative-time.ts
@@ -1,29 +1,13 @@
+import { SECOND } from "@/constants/time";
 import dayjs, { extend } from "dayjs";
 import duration from "dayjs/plugin/duration";
 import relativeTime from "dayjs/plugin/relativeTime";
-import updateLocale from "dayjs/plugin/updateLocale";
 
 export function humanizeRelativeTime(delta: number) {
+  if (delta >= -45 * SECOND && delta <= 0) return "now";
+
   extend(duration);
   extend(relativeTime);
-  extend(updateLocale);
-
-  dayjs.updateLocale("en", {
-    relativeTime: {
-      future: "in %s",
-      past: "%s ago",
-      m: "a minute",
-      mm: "%d minutes",
-      h: "an hour",
-      hh: "%d hours",
-      d: "a day",
-      dd: "%d days",
-      M: "a month",
-      MM: "%d months",
-      y: "a year",
-      yy: "%d years",
-    },
-  });
 
   return dayjs.duration(delta).humanize(true);
 }
