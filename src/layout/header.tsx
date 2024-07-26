962cead3205c1e4b18bc63229bc327f428afb9d0
CostasAK <costas@kokke.eu>
2024-07-26 21:27:40 +0200

    feat(header): click title to go home

--- src/layout/header.tsx
+++ src/layout/header.tsx
@@ -1,14 +1,18 @@
 import { Navigation } from "@/layout/navigation";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
     <header className="flex flex-col items-center bg-neutral-900 text-neutral-200">
-      <title className="flex items-center justify-center gap-2 p-2 sm:gap-4 sm:p-4">
+      <Link
+        to="/"
+        className="flex items-center justify-center gap-2 p-2 sm:gap-4 sm:p-4"
+      >
         <img src="/000047_hr1.png" alt="" className="size-8 sm:size-11"></img>
         <h1 className="text-4xl font-bold leading-none sm:text-5xl">
           FFXIV Buddy
         </h1>
-      </title>
+      </Link>
       <Navigation />
     </header>
   );
