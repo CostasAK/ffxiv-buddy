aa048350053db7acdabcc36c31c73b6cabfb88d4
CostasAK <costas@kokke.eu>
2024-07-26 21:06:28 +0200

    feat: back home button on error-page Fixes #59

--- src/pages/error.tsx
+++ src/pages/error.tsx
@@ -1,16 +1,23 @@
+import { Button } from "@/components/ui/button";
 import { useRouteErrorMessage } from "@/hooks/use-route-error-message";
+import Page from "@/layout/page";
+import { Link } from "react-router-dom";
 
 export default function ErrorPage() {
   const error = useRouteErrorMessage();
   console.error(error);
 
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
+    <Page className="flex w-fit flex-col gap-4">
       <p>Sorry, an unexpected error has occurred.</p>
       <p>
         <i>{error}</i>
       </p>
-    </div>
+      <Button asChild>
+        <Link to="/" reloadDocument>
+          Go Home
+        </Link>
+      </Button>
+    </Page>
   );
 }
